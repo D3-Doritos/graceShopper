d7c0a28799ef05cc76c776f75db1fae5966f201c
Andrew Gross <gross.andrewr@gmail.com>
2020-09-22 14:52:58 -0400

    feat: added db associations

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,6 +1,32 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
 const User = require('./user')
 const Product = require('./product')
 const Order = require('./order')
+
+const Cart = db.define('cart', {
+  qty: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {min: 0}
+  }
+})
+const Product_Order = db.define('product_order', {
+  qty: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {min: 0}
+  }
+})
+
+User.belongsToMany(Product, {through: Cart})
+Product.belongsToMany(User, {through: Cart})
+
+Order.belongsToMany(Product, {through: Product_Order})
+Product.belongsToMany(Order, {through: Product_Order})
+
+User.hasMany(Order)
+Order.belongsTo(User)
 
 /**
  * If we had any associations to make, this would be a great place to put them!
