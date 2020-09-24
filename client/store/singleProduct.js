2fac9defda312bd70f4b1bc84cc5572c7b594c4f
Pawan Benjamin <pawanbenjamin@gmail.com>
2020-09-24 17:40:38 -0400

    products editable by admin

--- client/store/singleProduct.js
+++ client/store/singleProduct.js
@@ -2,9 +2,14 @@ import axios from 'axios'
 
 // ACTIONS
 const GOT_PRODUCT = 'GOT_PRODUCT'
+const UPDATED_PRODUCT = 'UPDATED_PRODUCT'
 
 // ACTION CREATORS
 const gotProduct = product => ({type: GOT_PRODUCT, product})
+const updatedProduct = product => ({
+  type: UPDATED_PRODUCT,
+  product
+})
 
 // THUNK CREATORS
 export const fetchProduct = productId => async dispatch => {
@@ -16,12 +21,26 @@ export const fetchProduct = productId => async dispatch => {
   }
 }
 
+export const updateProduct = product => async dispatch => {
+  try {
+    const theUpdatedProduct = await axios.put(
+      `/api/products/${product.id}`,
+      product
+    )
+    dispatch(updatedProduct(theUpdatedProduct.data))
+  } catch (error) {
+    console.error(error)
+  }
+}
+
 // REDUCER
 const initialState = {}
 
 export default function(state = initialState, action) {
   switch (action.type) {
     case GOT_PRODUCT:
+      return action.product
+    case UPDATED_PRODUCT:
       return action.product
     default:
       return state
