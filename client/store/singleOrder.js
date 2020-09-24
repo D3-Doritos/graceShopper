523a201b5e8cd2541bdba72b11007c2db265e62b
taveershah <tanvirhussein@gmail.com>
2020-09-24 17:56:09 -0400

    feat: cart reducer added, yet to test

--- client/store/singleOrder.js
+++ client/store/singleOrder.js
@@ -2,15 +2,52 @@ import axios from 'axios'
 
 // ACTIONS
 const GET_ORDER = 'GET_ORDER'
+const ADD_PRODUCT = 'ADD_PRODUCT'
+const GET_CART = 'GET_CART'
+const REMOVE_PRODUCT = 'REMOVE_PRODUCT'
 
 // ACTION CREATORS
 const gotOrder = order => ({type: GET_ORDER, order})
+const addedProduct = cart => ({type: ADD_PRODUCT, cart})
+const gotCart = order => ({type: GET_CART, order})
+const removedProduct = cart => ({type: REMOVE_PRODUCT, cart})
 
 // THUNK CREATORS
 export const fetchOrder = orderId => async dispatch => {
   try {
     const order = await axios.get(`/api/order/${orderId}`)
     dispatch(gotOrder(order.data))
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export const addProduct = (orderId, productId) => async dispatch => {
+  try {
+    const cartProduct = await axios.put(
+      `/api/orders/${orderId}/addProduct/${productId}`
+    )
+    dispatch(addedProduct(cartProduct.data))
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export const getCart = userId => async dispatch => {
+  try {
+    const cart = await axios.get(`/api/users/${userId}/cart`)
+    dispatch(gotCart(cart.data))
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export const removeProduct = (orderId, productId) => async dispatch => {
+  try {
+    const updatedOrder = await axios.delete(
+      `/api/orders/${orderId}/deleteProduct/${productId}`
+    )
+    dispatch(removedProduct(updatedOrder))
   } catch (error) {
     console.error(error)
   }
@@ -23,6 +60,10 @@ export default function(state = initialState, action) {
   switch (action.type) {
     case GET_ORDER:
       return action.order
+    case ADD_PRODUCT:
+      return action.cart
+    case REMOVE_PRODUCT:
+      return action.cart
     default:
       return state
   }
